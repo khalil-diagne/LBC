c285e0ce7723ab891e2c14edce9e5193f0382f5b
khalil-diagne <diagneibeu10@gmail.com>
2026-05-02 15:57:58 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ sentransport/sentransport/src/ListeLignes.js
@@ -0,0 +1,21 @@
+import LigneBus from './LigneBus';
+import './ListeLignes.css';
+
+function ListeLignes({lignes}) {
+    return (
+        <div className="liste-lignes">
+        <div className='liste-titre'>Lignes Dakar Dem Dikk</div>
+        <p className = 'liste-description'>{lignes.length} lignes disponibles</p>
+        {lignes.map((ligne) => (
+            <LigneBus
+                key={ligne.id}
+                numero={ligne.numero}
+                depart={ligne.depart}
+                arrivee={ligne.arrivee}
+                arrets={ligne.arrets}
+            />
+        ))}
+        </div>
+    );
+}   
+export default ListeLignes;
