229d40b74e49945fb69536b228ab3411d5cdc456
khalil-diagne <diagneibeu10@gmail.com>
2026-05-02 13:31:38 +0000

    premier depot

---
+++ sentransport/sentransport/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className='App'>
+        <p> Bienvenue ! Cette application vous aide a trouver votre ligne de bus a dakar </p>
+      </main>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ sentransport/sentransport/src/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+function Header() {
+  return (
+    <header className="header">
+      <h1 className='header-titre'> Sentransport </h1>
+      <p className='header-soustitre'> Votre guide du transport en commun à Dakar </p>
+    </header>
+  );
+}
+
+export default Header;
